5a73735ebdfd6c6c502838216f8850b46fa58270
Aryan Moghariya <aryanmoghariya97@gmail.com>
2026-05-15 12:17:34 +0530

    policy done

---
+++ assets/js/policies.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  var links = $("#privacy-links a");
+  links.first().parent().addClass("active");
+
+  $(window).scroll(function () {
+    var fromTop = $(this).scrollTop();
+
+    links.each(function () {
+      var section = $($(this).attr("href"));
+
+      if (
+        section.position().top <= fromTop &&
+        section.position().top + section.outerHeight() > fromTop
+      ) {
+        links.each(function () {
+          $(this).parent().removeClass("active");
+        });
+        $(this).parent().addClass("active");
+      }
+    });
+  });
+});

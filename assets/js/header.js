314ffa0e26e97485e0366233fb95f334bfda6425
Aryan Moghariya <aryanmoghariya97@gmail.com>
2026-05-19 11:28:16 +0530

    header-hover-complate

---
+++ assets/js/header.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  function handleMegaMenu() {
+    const isMobile = $(window).width() <= 1025;
+
+    // Remove old events first
+    $(".header-link .nav-link").off("click");
+    $(".header-link").off("mouseenter mouseleave");
+
+    if (isMobile) {
+      $(".header-link").each(function () {
+        const menuItem = $(this);
+
+        // Skip if mega menu doesn't exist
+        if (menuItem.children(".mega-menu").length === 0) {
+          return;
+        }
+
+        menuItem.find(".nav-link").on("click", function (e) {
+          e.preventDefault();
+          e.stopPropagation();
+
+          $(".header-link").not(menuItem).removeClass("res-menu-active");
+
+          menuItem.toggleClass("res-menu-active");
+        });
+      });
+
+      $(".mega-menu").on("click", function (e) {
+        e.stopPropagation();
+      });
+
+      $(".main-header").removeClass("hover-active");
+    } else {
+      $(".header-link").removeClass("res-menu-active");
+
+      $(".header-link").on("mouseenter", function () {
+        $(".main-header").addClass("hover-active");
+      });
+
+      $(".header-link").on("mouseleave", function () {
+        $(".main-header").removeClass("hover-active");
+      });
+    }
+  }
+
+  handleMegaMenu();
+
+  $(window).on("resize", function () {
+    handleMegaMenu();
+  });
+});
